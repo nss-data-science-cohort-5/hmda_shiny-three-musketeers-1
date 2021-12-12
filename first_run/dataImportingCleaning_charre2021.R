f5c9372d8afcf1e9233c2f27fa291994d8e8c044
charre2021 <charre2021@outlook.com>
2021-12-11 22:26:21 -0600

    getting closer

--- first_run/dataImportingCleaning_charre2021.R
+++ first_run/dataImportingCleaning_charre2021.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(httr)
 library(jsonlite)
+library(splitstackshape)
 
 # Create queries for FFIEC API pull.
 hmda_queries <- list(states = "WA",
@@ -13,6 +14,72 @@ hmda_base_url <- c("https://ffiec.cfpb.gov/v2/data-browser-api/view/csv")
 hmda_request <- GET(url = hmda_base_url, query = hmda_queries)
 
 hmda <- read_csv(content(hmda_request))
+
+# Obtain LAR key for data codes. Downloaded from CFPB GitHub repol
+lar <- read_csv("data/lar.csv",  
+                col_select = c("Data Field Name", 
+                               "Descriptions and Examples",
+                               "Valid Values"))
+# Rename columns.
+names(lar) <- c("Name", "Descriptions", "VVs")
+
+# Just need to reduce the tibble down to variables for which categorical
+# numbers are used as "keys".
+lar <- lar %>% 
+  filter(grepl("^Description", Descriptions))
+
+# Split the valid values by spaces long-ways.
+lar <- cSplit(indt = lar, 
+              splitCols = "VVs", 
+              direction = "long", 
+              sep = " ")
+
+# Change valid values to character column.
+lar <- lar %>% 
+  mutate(VVs = as.character(VVs)) %>% 
+  filter(VVs != "1111")
+
+# Split "1.C" or "1.H" instances into "1. C" and "1. H".
+lar$Descriptions <- gsub("(\\d.)([A-Z])", "\\1 \\2", lar$Descriptions)
+
+# Compares pairs of values in two columns and extracts appropriate value
+# For each "key".
+for(i in (1:length(lar$Descriptions))) {
+  if(str_detect(lar$Descriptions[i], lar$VVs[i])) {
+    # Find a match? Pull out everything up to that point.
+    lar$Descriptions[i] <- str_extract(lar$Descriptions[i],
+                                        sprintf("(?<=%s.\\s).*",lar$VVs[i]))
+    # Then remove additional options afterwards.
+    lar$Descriptions[i] <- ifelse(str_detect(lar$Descriptions[i],
+                                              "[0-9]"),
+                                   str_extract(lar$Descriptions[i],
+                                               "^.+?(?=\\s\\d{1,2}\\.\\s)"),
+                                   lar$Descriptions[i])
+    # Then remove a couple extra sentences following certain options.
+    lar$Descriptions[i] <- ifelse(str_detect(lar$Descriptions[i],
+                                              "\\.\\s"),
+                                   str_extract(lar$Descriptions[i],
+                                               "^[A-Za-z]+?(?=\\.\\s)"),
+                                   lar$Descriptions[i])
+    
+  }
+}
+
+# Need to fix.
+find_a_match_in_lar <- function(cell, lar, guidepost) {
+  for(i in (1:length(lar$VVs))) {
+    if((lar$Name[i] == guidepost) && (cell == lar$VVs[i])) {
+      cell <- lar$Descriptions[i]
+    }
+  }
+  return(cell)
+}
+
+for(i in (1:length(lar$Name))){
+  hmda %>% 
+    select(contains(lar$Name[i])) %>% 
+    apply(., 1, function(x) find_a_match_in_lar(x, lar, lar$Name[i]))
+}
 
 # Method from .csv file, and then filter by US and only keep LEI and name.
 lei <- read_csv("data/gleif.csv",
@@ -27,6 +94,7 @@ filter(Entity.LegalAddress.Country == "US") %>%
 colnames(lei) <- c("LEI", "Entity Name")
 
 # Pull in API key and set census base URL.
+# Add tracts to census.
 api_key <- read_json("data/census.json")
 census_base_url <- c("https://api.census.gov/data/2019/acs/acs1/subject")
 
